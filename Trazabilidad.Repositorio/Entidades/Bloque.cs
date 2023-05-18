db5f1eed8f7b163993b5f8e5d9522f12f10363a0
ViktorAlbertoGuillin <viktorguillin@yahoo.com>
2023-05-18 07:37:56 -0300

    se hizo un retoque final con el texto del inicio y de la carga de infomacion

--- Trazabilidad.Repositorio/Entidades/Bloque.cs
+++ Trazabilidad.Repositorio/Entidades/Bloque.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace Trazabilidad.Repositorio.Entidades
     public class Bloque
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="Este Campo es obligatorio")]
         public string Datos { get; set; }
         public string Hash { get; set; }
         public string Hash_anterior { get; set; }
